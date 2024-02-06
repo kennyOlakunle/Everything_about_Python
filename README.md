a2e92aea5aade67f25b1e78209a4327a1e9727c2
Kehinde Abe <olakunlemuyiwa@gmail.com>
2024-02-06 17:38:10 +0000

    Adding some code

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Everything about Python
 This is a directory made for whoever is trying to learn Python from scratch.
+
+Check the Source folder i.e `src` for the code
--- src/input&print.py
+++ src/input&print.py
@@ -1,11 +1,12 @@
 print("Welcome to thw world of Python")
-print("What is the name of the city you grew in ?")
 
-city = input()
+city = input("What is the name of the city you grew in ?")
 
-print("What is the name of your best football player ?")
+# city = input()
 
-player = input()
+player = input("What is the name of your best football player ?")
+
+# player = input()
 
 print("The city you grew in is", city, "and your best football player is", player)
 
