1eb4ff931938717efba199c43f3029e91a16cfe6
Kehinde Abe <olakunlemuyiwa@gmail.com>
2024-02-06 17:31:00 +0000

    input and print

---
+++ src/input&print.py
@@ -0,0 +1,11 @@
+print("Welcome to thw world of Python")
+print("What is the name of the city you grew in ?")
+
+city = input()
+
+print("What is the name of your best football player ?")
+
+player = input()
+
+print("The city you grew in is", city, "and your best football player is", player)
+
